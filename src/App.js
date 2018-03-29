5090e1ba0bad9723ccb84c59d6708c9b8ca012da
Glenn Friedman <glennfriedman@glenns-mbp.home>
2018-03-29 12:03:21 -0400

    experimenting with home layout and electron

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  Alignment,
+  Button,
+  Navbar,
+  NavbarDivider,
+  NavbarGroup,
+  NavbarHeading,
+  Switch,
+  Card,
+  Elevation
+} from '@blueprintjs/core';
+import logo from './images/Pwc-logo-880x660.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar>
+          <NavbarGroup align={'left'}>
+            <NavbarHeading src={logo}>
+              <img style={{ height: '60px', padding: '2%' }} src={logo} alt="logo" />
+            </NavbarHeading>
+            <NavbarDivider />
+            <Button className="pt-minimal" icon="home" text="Home" />
+            <Button className="pt-minimal" icon="document" text="Files" />
+          </NavbarGroup>
+        </Navbar>
+        <div style={{ display: 'flex', flexDirection: 'column', margin: '2%' }}>
+          <Card elevation="4" interactive="true" style={{ margin: '1%' }}>
+            <h5>
+              <a href="#">2018 Sample Co. R&D Tax Credit Study</a>
+            </h5>
+            <Button className="pt-minimal" icon="document" text="See study" />
+          </Card>
+          <Card elevation="4" interactive="true" style={{ margin: '1%' }}>
+            <h5>
+              <a href="#">2018 Sample Co. R&D Tax Credit Study</a>
+            </h5>
+            <Button className="pt-minimal" icon="document" text="See study" />
+          </Card>
+          <Card elevation="4" interactive="true" style={{ margin: '1%' }}>
+            <h5>
+              <a href="#">2018 Sample Co. R&D Tax Credit Study</a>
+            </h5>
+            <Button className="pt-minimal" icon="document" text="See study" />
+          </Card>
+          <Card elevation="4" interactive="true" style={{ margin: '1%' }}>
+            <h5>
+              <a href="#">2018 Sample Co. R&D Tax Credit Study</a>
+            </h5>
+            <Button className="pt-minimal" icon="document" text="See study" />
+          </Card>
+        </div>
       </div>
     );
   }
